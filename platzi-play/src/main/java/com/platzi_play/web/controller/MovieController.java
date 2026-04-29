1c74f5f588f5d1939cb730319e075f0d18f93757
Daniela-Mendez27 <camachodanim27@gmail.com>
2026-04-28 19:48:50 -0600

    separation of domain

--- platzi-play/src/main/java/com/platzi_play/web/controller/MovieController.java
+++ platzi-play/src/main/java/com/platzi_play/web/controller/MovieController.java
@@ -1,22 +1,24 @@
 package com.platzi_play.web.controller;
 
-import com.platzi_play.persistence.entity.MovieEntity;
+import com.platzi_play.domain.dto.MovieDto;
+import com.platzi_play.domain.service.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.platzi_play.persistence.crup.CrudMovieEntity;
+
 
 import java.util.List;
 
 @RestController
 public class MovieController {
-    private final CrudMovieEntity crudMovieEntity;
+   private  final MovieService movieService;
 
-    public MovieController(CrudMovieEntity crudMovieEntity){
-        this.crudMovieEntity = crudMovieEntity;
+   public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+
     }
     @GetMapping("/movies") //creación de endpoint
-    public List<MovieEntity> getAll(){
-        return  this.crudMovieEntity.findAll();
+    public List<MovieDto> getAll(){
+        return  this.movieService.getAll();
     }
 
 }
