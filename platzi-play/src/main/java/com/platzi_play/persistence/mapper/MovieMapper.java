aa970fc36a01e09dfec8d4809c46c6fe01be3cba
Daniela-Mendez27 <camachodanim27@gmail.com>
2026-04-30 19:20:10 -0600

    MapStruct for enums and strings

---
+++ platzi-play/src/main/java/com/platzi_play/persistence/mapper/MovieMapper.java
@@ -0,0 +1,23 @@
+package com.platzi_play.persistence.mapper;
+
+
+import com.platzi_play.domain.dto.MovieDto;
+import com.platzi_play.persistence.entity.MovieEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses ={GenreMapper.class})
+public interface MovieMapper {
+
+
+    @Mapping(source = "titulo", target = "title")
+    @Mapping(source = "duracion", target = "duration")
+    @Mapping(source = "genero", target = "genre", qualifiedByName = "stringToGenre" )
+    @Mapping(source = "fechaEstreno", target = "releaseDate")
+    @Mapping(source = "clasificacion", target = "rating")
+    MovieDto toDto(MovieEntity entity);
+    List<MovieDto> toDtos(Iterable<MovieEntity> entities);
+}
